13a3a4d1e81111398b9f9d5b7b84ff16d706376f
Jeevan D C <jeevu.art@gmail.com>
2017-02-05 01:31:57 +0530

    Update getPagesSource.js

--- predictextension/predictextension/getPagesSource.js
+++ predictextension/predictextension/getPagesSource.js
@@ -1,16 +1,9 @@
 var messArr ;
 function start(document) {
-
-        
         var message = window.getSelection().toString();
-
-        
-
             var xmlhttp = new XMLHttpRequest();
             //var url = "http://127.0.0.1:5000/q/"+message;
-            var url = "http://noobg1.pythonanywhere.com/q/" + message;
-
-
+            var url = "http://noobg1.pythonanywhere.com/q/" + message
         return url;
         
 }
